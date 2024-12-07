3ecfea7b1a1ff8de1b76de6d22cd8fc006994302
dimasiggs <dima.dashkin@bk.ru>
2024-12-07 19:17:54 +0500

    feat: Добавил запросы "GET /formula/similarity", "GET /formula/similar_formulas", "POST /formula" в api

--- app/api.py
+++ app/api.py
@@ -4,23 +4,31 @@ from fastapi import (
 
 from fuzzywuzzy import fuzz
 
-
+import database
 
 api = FastAPI()
+
+Formulas = database.Formulas("database.db")
+
+
+def formulas_similarity(f1: str, f2: str) -> int:
+    return fuzz.ratio(f1, f2) # TODO: МБ переделать алгоритм определения сходства формул
 
 
 @api.get("/formula/similarity")
 async def get_formulas_similarity_percentage(formula1, formula2) -> int:
-    return fuzz.ratio(formula1, formula2) # TODO: МБ переделать алгоритм определения сходства формул
+    return formulas_similarity(formula1, formula2)
 
 
-@api.get("/formula/similar")
+@api.get("/formula/similar_formulas")
 async def get_similar_formulas_in_db(formula) -> list:
-    return [formula] * 10 # TODO: Возвращать похожие формулы из бд
+    result = []
+    for i in Formulas.get_all_formulas():
+        if formulas_similarity(formula, i) >= 70:
+            result.append(i)
+    return result
 
 
 @api.post("/formula")
 async def add_formula(formula) -> None:
-    ... # TODO: Добавить формулу в бд
-
-
+    Formulas.add_formula(formula)
---
+++ app/database.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+class Formulas:
+    def __init__(self, database_name: str) -> None:
+        connection = sqlite3.connect(database_name)
+        self._connection = connection
+
+    def get_all_formulas(self) -> list:
+        return self._connection.execute("""SELECT formula FROM Formulas""").fetchall()
+
+    def add_formula(self, formula: str) -> None:
+        self._connection.execute("""INSERT INTO Formulas (formula) VALUES (?)""", (formula,))
+        self._connection.commit()
