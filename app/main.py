2bb4615e6a9369b8f7136aeb55be600ed53503ba
dimasiggs <dima.dashkin@bk.ru>
2024-12-07 17:10:54 +0500

    build: добавил запуск через docker-compose

--- app/main.py
+++ app/main.py
@@ -9,5 +9,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root() -> HTMLResponse:
-    html_main_page = open(f"frontend/index.html", "r", encoding="utf-8").read()
-    return HTMLResponse(content=html_main_page, status_code=200)
+    return HTMLResponse(content="html_main_page!", status_code=200)
