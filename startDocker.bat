228ee3de85e37ea5af258f81f0ff12bd7510eec6
dimasiggs <dima.dashkin@bk.ru>
2024-12-07 18:52:09 +0500

    build: изменил startDocker.bat

--- startDocker.bat
+++ startDocker.bat
@@ -1,4 +1,2 @@
-docker container kill mycontainer
-docker container rm mycontainer
-docker build -t myimage .
-docker run -d --name mycontainer -p 80:80 myimage
+docker compose build
+docker compose up
