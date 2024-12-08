c85c6419d7cad640705fdeec321926221faafd7b
dimasiggs <dima.dashkin@bk.ru>
2024-12-09 01:17:29 +0500

    feat: изменил оценку схожести формул. "GET /formula/similarity" возвращает раскрашенные формулы и процент сходства

--- app/api.py
+++ app/api.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import (
     FastAPI,
 )
@@ -11,12 +13,27 @@ api = FastAPI()
 Formulas = database.Formulas("database.db")
 
 
-def formulas_similarity(f1: str, f2: str) -> int:
-    return fuzz.ratio(f1, f2) # TODO: МБ переделать алгоритм определения сходства формул
+def formulas_similarity(s1, s2, l=0) -> dict:
+    q1 = s1
+    q2 = s2
+    if len(s1) > len(s2):
+        q1 = s2
+        q2 = s1
+
+    for start in range(len(q1)):
+        for end in range(len(q1), start + l, -1):
+            substring = q1[start:end]
+            if substring in q2:
+                q2 = q2.replace(substring, "")
+                color = hex(random.randrange(0, 2 ** 24))[2:]
+                s1 = s1.replace(substring, "\colorbox{#" + color + "}{" + substring + "}")
+                s2 = s2.replace(substring, "\colorbox{#" + color + "}{" + substring + "}")
+
+    return {"string1": s1, "string2": s2, "percent": fuzz.ratio(s1, s2)}
 
 
 @api.get("/formula/similarity")
-async def get_formulas_similarity_percentage(formula1, formula2) -> int:
+async def get_formulas_similarity_percentage(formula1, formula2) -> dict:
     return formulas_similarity(formula1, formula2)
 
 
@@ -24,7 +41,7 @@ async def get_formulas_similarity_percentage(formula1, formula2) -> int:
 async def get_similar_formulas_in_db(formula) -> list:
     result = []
     for i in Formulas.get_all_formulas():
-        if formulas_similarity(formula, i) >= 70:
+        if formulas_similarity(formula, i)["percent"] >= 70:
             result.append(i)
     return result
 
