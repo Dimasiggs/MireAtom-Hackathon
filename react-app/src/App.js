236475cdc1d073c7566ac5d17f5fdbfcbb0fe0fa
Timur-Is <t_faktiv_t@mail.ru>
2024-12-13 23:21:22 +0500

    feat(webpage): накидал скелет страницы

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,21 @@ function App() {
     return (
         <>
             <main>
+                <h1>Редактор и анализатор формул</h1>
+                <section>
+                    <h2>Формула 1</h2>
+                    <div>Формула</div>
+                    <div>LaTeX</div>
+                </section>
+                <section>
+                    <h2>Формула 2</h2>
+                    <div>Формула</div>
+                    <div>LaTeX</div>
+                </section>
+                <section>
+                    <h2>Совпадение формул</h2>
+                    <div>Формула (неинтерактивная)</div>
+                </section>
             </main>
         </>
     );
