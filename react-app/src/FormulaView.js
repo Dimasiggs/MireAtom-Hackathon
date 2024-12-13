5a9807fa50714c2abcb857b6037b4cf3185ddbfa
Timur-Is <t_faktiv_t@mail.ru>
2024-12-14 00:18:26 +0500

    feat(webpage): сделал `FormulaView` интерактивным

--- react-app/src/FormulaView.js
+++ react-app/src/FormulaView.js
@@ -1,10 +1,47 @@
+import { TextField, Label, TextArea } from 'react-aria-components';
+import 'mathlive';
+import 'mathlive/fonts.css';
+
 function FormulaView({ latex, setLatex }) {
+    const mathFieldStyle = {
+        display: 'block',
+    };
+
+    if (setLatex == null) return (
+        <>
+            <math-field
+                contenteditable='false'
+                tabIndex='0'
+                style={mathFieldStyle}
+            >
+                {latex}
+            </math-field>
+        </>
+    );
+
+    const onInput = (e) => setLatex(e.target.value);
+
     return (
         <>
-            <div>{latex}</div>
-            <div>LaTeX</div>
+            <math-field
+                contenteditable='true'
+                tabIndex='0'
+                onInput={onInput}
+                style={mathFieldStyle}
+            >
+                {latex}
+            </math-field>
+
+            <TextField value={latex} onChange={setLatex}>
+                <Label>LaTeX</Label>
+                <TextArea style={{
+                    resize: 'vertical',
+                    display: 'block',
+                    width: '100%',
+                }} />
+            </TextField>
         </>
-    )
+    );
 }
 
 
