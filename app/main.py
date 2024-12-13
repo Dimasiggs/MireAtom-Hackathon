770b040920615dce5e790ec831cfc831e9d81b6a
dimasiggs <dima.dashkin@bk.ru>
2024-12-14 03:10:46 +0500

    build: отправка html и static files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import (
   FastAPI,
 )
+from starlette.responses import (
+  FileResponse,
+)
+from starlette.staticfiles import StaticFiles
+
 from app.api import api as api_v01
 
 app = FastAPI()
 app.mount("/api/v01", api_v01)
+app.mount("/static", StaticFiles(directory="/code/build/static", html = True), name="static")
+
+@app.get("/")
+def main_html() -> FileResponse:
+  return FileResponse("/code/build/index.html", status_code=200)
