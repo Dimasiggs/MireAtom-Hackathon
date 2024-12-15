e300775d686f43c4e05b60b65b87ae527ad96eac
dimasiggs <dima.dashkin@bk.ru>
2024-12-16 00:55:04 +0500

    feat: новое сравнение формул

--- app/api.py
+++ app/api.py
@@ -1,11 +1,7 @@
-import json
-import random
-
 from fastapi import (
     FastAPI,
 )
 
-import ast
 from fuzzywuzzy import fuzz
 #from pix2tex.cli import LatexOCR
 
@@ -21,25 +17,67 @@ Formulas = database.Formulas("database.db")
 
 
 # Функция для сравнения формул на схожесть
-def formulas_similarity(s1, s2, l=2) -> dict:
-    q1 = s1
-    q2 = s2
-    if len(s1) > len(s2):
-        q1 = s2
-        q2 = s1
+import fuzzywuzzy.fuzz
+import sympy
+from sympy.parsing.latex import parse_latex
 
-    # Проходимся по всем подстрокам и записываем все совпавшие
-    for start in range(len(q1)):
-        for end in range(len(q1), start + l, -1):
-            substring = q1[start:end]
-            if substring in q2:
-                q2 = q2.replace(substring, "")
 
-                s1 = s1.replace(substring, r"\colorbox{#88E788}{$" + substring + "$}")
-                s2 = s2.replace(substring, r"\colorbox{#88E788}{$" + substring + "$}")
+def expr_to_nested_list(expr, max_depth: int, current_depth: int = 0):
+    if expr.is_Atom or current_depth >= max_depth:
+        return [expr]
+    args = [expr_to_nested_list(arg, max_depth, current_depth + 1) for arg in expr.args]
 
-    return {"string1": s1, "string2": s2, "percent": fuzz.ratio(s1, s2)}
+    return args
 
+
+def flatten(nested_list) -> list:
+    result = []
+    for item in nested_list:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+
+def expression_all_subtrees(ex: sympy.exp, min_len: int) -> list:
+    depth = 0
+    subtrees = []
+    _tree = []
+    while 1:
+        tree = expr_to_nested_list(ex, depth)
+        tree = set(flatten(tree))
+        if tree == _tree:
+            break
+
+        subtrees += [a for a in tree if len(str(a).replace(" ", "")) >= min_len]
+
+        _tree = tree
+        depth += 1
+
+    return list(set(subtrees))
+
+
+def formulas_similarity(s1, s2, min_len):
+    expr1 = parse_latex(s1)
+    expr2 = parse_latex(s2)
+
+    subtrees1 = expression_all_subtrees(expr1, min_len)
+    subtrees2 = expression_all_subtrees(expr2, min_len)
+    expressions = []
+
+    for i in subtrees1:
+        for j in subtrees2:
+            if i.equals(j):
+                expressions.append((i, j))
+
+    latex_expr1 = str(sympy.latex(expr1))
+    latex_expr2 = str(sympy.latex(expr2))
+    for i in expressions:
+        latex_expr1 = latex_expr1.replace(sympy.latex(i[0]), r"\colorbox{#88E788}{" + str(sympy.latex(i[0])) + "}")
+        latex_expr2 = latex_expr2.replace(sympy.latex(i[1]), r"\colorbox{#88E788}{" + str(sympy.latex(i[1])) + "}")
+
+    return {"s1": latex_expr1, "s2": latex_expr2, "percent": fuzz.ratio(s1, s2)}
 
 
 @api.get("/formula/img_to_latex")
@@ -50,7 +88,7 @@ async def get_latex_from_img(img) -> str:
 # Получить схожесть введенных формул
 @api.get("/formula/similarity")
 async def get_formulas_similarity_percentage(formula1, formula2) -> dict:
-    return formulas_similarity(formula1, formula2)
+    return formulas_similarity(formula1, formula2, 4)
 
 
 # TODO: переделать с новым сравнением на схожесть
